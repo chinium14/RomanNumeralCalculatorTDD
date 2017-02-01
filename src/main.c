e98dc7760af6e9216949319e25254be83e46b4df
Shuai Wei <ceshwei@gmail.com>
2017-02-01 11:44:01 -0500

    Set up Makefile using Autotools.

---
+++ src/main.c
@@ -0,0 +1,12 @@
+/****************************************************/
+/*This project is the coding exercise using TDD by  */
+/*Shuai Wei 2/1/2017 using the C language with check*/
+/*This file is the main function.                   */    
+/****************************************************/
+
+#include <stdio.h>
+#include <check.h>
+int main(int argc, char* argv[])
+{
+   return 0;
+}
